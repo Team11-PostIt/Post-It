33c48123e19288c86d203fac9b93525edf6671d9
minji <ipink89@naver.com>
2022-11-13 21:57:19 +0900

    333 commit

--- app/src/main/java/com/example/sns_postit/SuccessActivity2.kt
+++ app/src/main/java/com/example/sns_postit/SuccessActivity2.kt
@@ -1,11 +1,20 @@
 package com.example.sns_postit
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class SuccessActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.success)
+
+        var btn : Button = findViewById<Button>(R.id.LogOut)  //회원가입 버튼 누르면 회원가입 창 이동
+
+        btn.setOnClickListener() {
+            val intent = Intent (this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
