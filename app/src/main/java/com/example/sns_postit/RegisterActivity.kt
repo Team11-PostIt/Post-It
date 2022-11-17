fb8780ae8fc15a748d9ec5d10115a415fe6da38f
leeyeeun <lye0308@iCloud.com>
2022-11-17 16:43:34 +0900

    first commit

--- app/src/main/java/com/example/sns_postit/RegisterActivity.kt
+++ app/src/main/java/com/example/sns_postit/RegisterActivity.kt
@@ -1,29 +1,52 @@
 package com.example.sns_postit
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 
 class RegisterActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
+        val db = Firebase.firestore
 
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-
-        var btn : Button = findViewById<Button>(R.id.write_button)  // 로그아웃 버튼, 로그인 창으로 돌아감
+        var btn : Button = findViewById<Button>(R.id.write_button)  // 등록하기 버튼, 작성한 글 등록됨
+        val etTitle:EditText = findViewById<EditText>(R.id.title_et)
+        val etContent:EditText = findViewById<EditText>(R.id.content_et)
 
         btn.setOnClickListener() {
             val intent = Intent (this, ListActivity::class.java)
             startActivity(intent)
+
+            //1.레퍼런스 가져오기
+            val col = db.collection("post") //post컬렉션
+
+
+            //2.접근하기(쓰기)
+            val postTitle = etTitle.getText().toString()//게시글 작성에서 쓴 게시글이름
+            val itemMap = hashMapOf(
+                "title" to postTitle, //title필드에 게시글 이름 저장
+                "content" to etContent.getText().toString() //content필드에 게시글 내용 저장
+            )
+            col.document(postTitle).set(itemMap) // 문서이름으로 document생성
+
+
+            //3. 접근하기(읽기)
+            col.addSnapshotListener{ value,error->
+                for(d in value!!.documentChanges){
+                    println("${d.type},${d.document.id},${d.document.data["text"]}")
+                }
+            }
+
+
+
         }
-
-
-
-
     }
 }
 
